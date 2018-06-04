a3fde8372e285aa745b00263464aaec9c9011ea0
Leonardo de Carvalho <leocarvalho001@gmail.com>
2018-06-04 12:35:31 -0300

    dont upgrade to v5 yet

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -82,7 +82,7 @@ const size_t   FUSION_TX_MIN_IN_OUT_COUNT_RATIO              = 4;
 const uint32_t UPGRADE_HEIGHT_V2                             = 18500;
 const uint32_t UPGRADE_HEIGHT_V3                             = 40000;
 const uint32_t UPGRADE_HEIGHT_V4                             = 100000;
-const uint32_t UPGRADE_HEIGHT_V5                             = 200000;
+const uint32_t UPGRADE_HEIGHT_V5                             = 2147483645;
 
 const unsigned UPGRADE_VOTING_THRESHOLD                      = 90; // percent
 const uint32_t UPGRADE_VOTING_WINDOW                         = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;  // blocks
